e7facefad6c89ab1d331f4f642b99bc61b7eddc4
Ben Kogan <benkogan@me.com>
2015-03-04 23:26:27 -0500

    Add more methods, tests

--- flash.rb
+++ flash.rb
@@ -1,27 +1,128 @@
+#!/usr/bin/env ruby
+
+require "optparse"
 require "json"
 
 class Flash
+  VERSION = [0, 0, 2]
+
   def main
-    # . . .
+    options = Options.parse ARGV
+    collection = Collection.new(options.collection_path)
 
-    options = Configuration.parse_options(ARGV) # TODO
-
-    search_groups = Configuration.parse_search(options[:query])
-    collection = Collection.new(file_path).load # TODO
-    notes = collection.filter(search_groups) # TODO
-    cards = Collection.build_cards(notes) # TODO
-
-    # . . .
-    cards
+    if options.command = 'add'
+      Screen.with_screen do |screen, tty|
+        Add.run_in_screen(collection, options.unit, screen, tty)
+      end
+    elsif options.command = 'review'
+      search = Search.parse(options.query)
+      notes = collection.filter_by(search)
+      cards = collection.build_cards(notes)
+      Screen.with_screen do |screen, tty|
+        Quiz.new(options.mode).run_in_screen(cards, screen, tty)
+      end
+    end
   end
 
-  def quiz
+end
+
+class Add
+
+  def self.run_in_screen(collection, unit, screen, tty)
+    # add unit to collection
+    # pass to event loop? or use event loop block?
+  end
+
+  def self.ui_event_loop
+  end
+
+end
+
+class Options
+  def self.parse(argv)
+    options = OpenStruct.new
+    options.collection_path = nil
+    options.query = nil
+    options.mode = :srs
+    options.unit = nil
+    options.command = nil
+
+    global = OptionParser.new do |opts|
+      opts.banner = "Usage: #{$PROGRAM_NAME} [options] [commands]"
+
+      opts.on_tail("-h", "--help", "Show this message") do |v|
+        puts opts
+        puts
+        puts "TODO: commands list"
+        puts "TODO: how to get help for each command (command -h"
+        exit
+      end
+
+      opts.on_tail("-v", "--version", "Show version") do
+        puts Flash::VERSION.join('.')
+        exit
+      end
+    end
+
+    subcommands = {
+      "review" => OptionParser.new do |opts|
+        opts.banner = command_banner("review")
+
+        opts.on("-qQUERY", "--query=QUERY", "Search for units and tags") do |q|
+          options.query = q
+        end
+
+        opts.on("--cram", "Toggle cram review mode (default)") do |o|
+          options.mode = :cram
+        end
+
+        opts.on("--srs", "Toggle SRS review mode") do |o|
+          options.mode = :srs
+        end
+
+        opts.on_tail("-h", "--help", "Show this message") do |v|
+          puts opts
+          exit
+        end
+      end,
+
+      "add" => OptionParser.new do |opts|
+        opts.banner = command_banner("add")
+
+        opts.on("-uUNIT", "--unit=UNIT", "Specify unit to add to") do |u|
+          options.unit = u
+        end
+
+        opts.on_tail("-h", "--help", "Show this message") do |v|
+          puts opts
+          exit
+        end
+      end
+    }
+
+    begin
+      global.order!
+      subcommand = argv.shift
+      options.command = subcommand
+      options.collection_path = argv.shift
+      subcommands[subcommand].order!
+    rescue OptionParser::InvalidOption => e
+      $stderr.puts e
+      $stderr.puts global
+      exit 1
+    end
+
+    options
+  end
+
+  def self.command_banner(subcommand)
+    "Usage: #{$PROGRAM_NAME} #{subcommand} [options]"
   end
 end
 
-class Configuration
-
-  def self.parse_search(query)
+class Search
+  # TODO: what if query is empty
+  def self.parse(query)
     begin
       query_atoms = query.split
       query_atoms.map { |atom| parse_query_atom atom }
@@ -37,46 +138,83 @@ class Configuration
   #   => unit[tag1,tag2,tag3]
 
   def self.parse_query_atom(atom)
-    word_symb = '\w+'
-    tags_symb = '\[(\w+(?:,\w+)*)\]'
+    unit_symb = '(?<unit>\w+)'
+    tags_symb = '(?<tags>\w+(?:,\w+)*)'
+    atom_symb = /^#{unit_symb}(?:\[#{tags_symb}\])?$/
 
-    unit_pattern = /#{word_symb}/
-    tags_pattern = /#{tags_symb}/
-    query_pattern = /^#{word_symb}(?:#{tags_symb})?$/
+    matched = atom_symb.match(atom)
+    raise InvalidQueryAtomError if matched.nil?
 
-    raise InvalidQueryAtomError if query_pattern.match(atom).nil?
-
-    unit = unit_pattern.match(atom)
-    tags = tags_pattern.match(atom)
-
-    # extract data if there are matches
-    unit = unit.to_a.pop if unit
-    tags = tags[1].split(',') if tags
-
-    {unit: unit, tags: tags}
+    OpenStruct.new(
+      unit: matched[:unit],
+      tags: matched[:tags] ? matched[:tags].split(',') : nil
+    )
   end
 
   class InvalidQueryAtomError < RuntimeError; end
-
 end
 
 class Collection
-
   def initialize(file_path)
-    @file_path = file_path
+    @path = file_path
+    @data = File.read(@path)
+    @json = JSON.parse(@data)
+
+    @definition = @json.fetch('definition')
+    @notes = @json.fetch('notes')
   end
 
-  def load
-    collection_file = file.load @file_path
-    collection_file # TODO
+  # TODO: what if filter returns nothing?
+  # NOTE: if no tags specified for a group, returns all notes
+  def filter_by(search_groups)
+    filtered_notes = search_groups.map do |group|
+      if group.tags
+        filter_by_tag(@notes[group.unit], group.tags)
+      else
+        @notes[group.unit]
+      end
+    end
+    filtered_notes.flatten
   end
 
-  def filter(search_groups)
+  def filter_by_tag(notes, tags)
+    notes.select do |note|
+      intersection?(tags, note["tags"])
+    end
   end
 
+  def intersection?(first, second)
+    overlap = second.select { |x| first.include? x }
+    overlap.any?
+  end
+
+  def build_cards(notes)
+    cards = notes.map do |note|
+      @definition.map do |ordering_style, sides|
+        build_card(sides, note)
+      end
+    end
+    cards.flatten
+  end
+
+  def build_card(sides, note)
+    sides = sides.map do |side|
+      side.map do |key|
+        # new fact
+        OpenStruct.new(
+          key: key,
+          val: note[key]
+        )
+      end
+    end
+    Card.new(sides)
+  end
 end
 
 class Card
+  def initialize(sides)
+    @sides = sides
+  end
 end
 
 class Renderer
@@ -90,4 +228,6 @@ end
 
 class TTY
 end
+
+Flash.new.main if $0 == __FILE__
 
--- test.rb
+++ test.rb
@@ -1,49 +1,53 @@
 require_relative "flash"
 require "minitest/autorun"
 
-describe Configuration do
-
+describe Search do
   describe "#parse_query_atom" do
-
     it "must parse a unit" do
-      group = Configuration.parse_query_atom 'unit'
+      group = Search.parse_query_atom 'unit'
       group[:unit].must_equal 'unit'
     end
 
     it "must parse a unit before a tag" do
-      group = Configuration.parse_query_atom 'unit[a]'
+      group = Search.parse_query_atom 'unit[a]'
       group[:unit].must_equal 'unit'
     end
 
     it "must parse a unit and single tag" do
-      group = Configuration.parse_query_atom 'unit[a]'
+      group = Search.parse_query_atom 'unit[a]'
       group[:tags].must_equal ['a']
     end
 
     it "must parse a unit and multiple tags" do
-      group = Configuration.parse_query_atom 'unit[a,b,c]'
+      group = Search.parse_query_atom 'unit[a,b,c]'
       group[:tags].must_equal ['a', 'b', 'c']
     end
 
     it "must complain about bogus atoms" do
       bogus_query_atoms = ['test[', '[a]', '[a,b]', ']a&dsf[$&adsf^,ds,[]']
       bogus_query_atoms.each do |atom|
-        assert_raises(Configuration::InvalidQueryAtomError) do
-          Configuration.parse_query_atom atom
+        assert_raises(Search::InvalidQueryAtomError) do
+          Search.parse_query_atom atom
         end
       end
     end
-
   end
 
-  describe "#parse_query" do
-
+  describe "#parse" do
     it "must parse a query" do
-      search = Configuration.parse_search 'u1 u2[a,b,c]'
-      search.must_equal [{:unit=>"u1", :tags=>nil}, {:unit=>"u2", :tags=>["a", "b", "c"]}]
+      search = Search.parse 'u1 u2[a,b,c]'
+
+      group = search.shift
+      group.unit.must_equal "u1"
+      group.tags.must_be_nil
+
+      group = search.shift
+      group.unit.must_equal "u2"
+      group.tags.must_equal ["a", "b", "c"]
     end
-
   end
+end
 
+describe Collection do
 end
 
